e4fd15a472f6d16a937f2975a2a56378f4756dfe
Julian Nymark <julianymark@gmail.com>
2016-12-07 19:43:03 +0100

    even more sparkles

--- tierlistscraper.py
+++ tierlistscraper.py
@@ -7,14 +7,15 @@ import json
 url = 'http://www.heartharena.com/tierlist'
 
 soup = ""
+cards = {}
 
 tierfile = './tierlist.json'
 tierlist_html = Path(tierfile)
 
 if os.path.isfile(tierfile) and os.stat(tierfile).st_size > 0:
     f = open(tierfile, 'r')
-    json_card_values = f.read()
-    card_values = json.loads(json_card_values)
+    json_cards_info = f.read()
+    cards_info = json.loads(json_cards_info)
 else:
     f = open(tierfile, 'w')
     response = urllib.request.urlopen(url)
@@ -22,22 +23,23 @@ else:
     soup = BeautifulSoup(html, 'html.parser')
     cards = soup.find_all(class_="card")
 
-    card_values = {}
+    cards_info = {}
 
     for card in cards:
         if card.dt != None and card.dd != None:
+            card_extra = card.dt['class']
             card_name = card.dt.contents[0].strip().lower()
             card_cost = int(card.dd.contents[0].strip())
-            card_values[card_name] = card_cost
+            cards_info[card_name] = ( card_cost, card_extra )
 
-    f.write(json.dumps(card_values))
+    f.write(json.dumps(cards_info))
 
 print("\n#### pick these:\n")
-for name, value in sorted(card_values.items(), key=lambda tup: tup[1]):
-    if value <= 10:
-        print(name, value)
+for name, value in sorted(cards_info.items(), key=lambda tup: tup[1][0]):
+    if value[0] <= 10:
+        print(name.ljust(30), value)
 
 print("\n#### absolute garbage (do not touch):\n")
-for name, value in sorted(card_values.items(), key=lambda tup: tup[1], reverse=True):
-    if value >= 100:
-        print(name, '\t', value)
+for name, value in sorted(cards_info.items(), key=lambda tup: tup[1][0], reverse=True):
+    if value[0] >= 100:
+        print(name.ljust(30), value)
